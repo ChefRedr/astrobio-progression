d068b3861fe7d4a42e0be8e1e188fe42875816d3
joannae05 <joanna.here05@gmail.com>
2025-10-04 21:20:38 -0500

    Filled in structure for dashboard page

---
+++ astrobio-progression/src/components/Pages/Dashboard/BarGraph/BarGraph.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BarGraph.css"
+
+function BarGraph() {
+    return (
+        <div className="main-container">
+        </div>
+    );
+}
+export default BarGraph;
---
+++ astrobio-progression/src/components/Pages/Dashboard/WordGraph/WordGraph.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./WordGraph.css"
+
+function WordGraph() {
+    return (
+        <div className="main-container">
+        </div>
+    );
+}
+export default WordGraph;
